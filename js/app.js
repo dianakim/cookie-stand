ec1a632ade5199c0a1145db9010932a9830aafad
Diana Kim <dianajkim@gmail.com>
2019-08-24 15:05:15 -0700

    shops' random number properties added to all

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
 function random(min, max){
   var randNum = Math.random() * (+max - +min) +  +min;
   return Math.floor(randNum);
-
 }
 
 //hours open are 6am to 8pm
@@ -17,38 +16,66 @@ var firstAndPike = {
   customersPerHour: function(){
     return random(this.minCustomerPerDay, this.maxCustomerPerDay);
   },
-
   cookiesPerHour: function(){
     return this.customersPerHour() * this.avgCookiesPerSale;
   }
-}
-console.log(`Customers per hour is ${firstAndPike.customersPerHour()}`);
-console.log(`Cookies per hour is ${firstAndPike.cookiesPerHour()}.`);
+};
 
 var seaTac = {
   name: 'SeaTac Airport',
   minCustomerPerDay: 3,
   maxCustomerPerDay: 24,
-  avgCookiesPerSale: 1.2
-}
+  avgCookiesPerSale: 1.2,
+  customersPerHour: function(){
+    return random(this.minCustomerPerDay, this.maxCustomerPerDay);
+  },
+  cookiesPerHour: function(){
+    return this.customersPerHour() * this.avgCookiesPerSale;
+  }
+};
 
 var seattleCenter  = {
   name: 'Seattle Center',
   minCustomerPerDay: 11,
   maxCustomerPerDay: 38,
-  avgCookiesPerSale: 3.7
-}
+  avgCookiesPerSale: 3.7,
+  customersPerHour: function(){
+    return random(this.minCustomerPerDay, this.maxCustomerPerDay);
+  },
+  cookiesPerHour: function(){
+    return this.customersPerHour() * this.avgCookiesPerSale;
+  }
+};
 
 var capitolHill = {
   name: 'Capitol Hill',
   minCustomerPerDay: 20,
   maxCustomerPerDay: 38,
-  avgCookiesPerSale: 2.3
-}
+  avgCookiesPerSale: 2.3,
+  customersPerHour: function(){
+    return random(this.minCustomerPerDay, this.maxCustomerPerDay);
+  },
+  cookiesPerHour: function(){
+    return this.customersPerHour() * this.avgCookiesPerSale;
+  }
+};
 
 var alki = {
   name: 'Alki',
   minCustomerPerDay: 2,
   maxCustomerPerDay: 16,
-  avgCookiesPerSale: 4.6
+  avgCookiesPerSale: 4.6,
+  customersPerHour: function(){
+    return random(this.minCustomerPerDay, this.maxCustomerPerDay);
+  },
+  cookiesPerHour: function(){
+    return this.customersPerHour() * this.avgCookiesPerSale;
+  }
+};
+
+var shops = [firstAndPike, seaTac, seattleCenter, capitolHill, alki];
+
+for(var i = 0; i < shops.length; i++){
+  console.log(`Customers per hour is ${shops[i].customersPerHour()}`);
+  console.log(`Cookies per hour is ${shops[i].cookiesPerHour()}.`);
 }
