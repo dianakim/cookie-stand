c28f352b19e9dfebcc2ffe95f0d2c8310a66fa3f
Diana Kim <dianajkim@gmail.com>
2019-08-30 22:00:51 -0700

    Calculated and filled cookies sold each hour array

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var tableEl = document.getElementById('sales-table');
+
 var allShops = [];
 var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
@@ -14,36 +15,15 @@ function Shop(name, minCustomer, maxCustomer, avgCookiesPerSale) {
   allShops.push(this);
 }
 
-Shop.prototype.render = function() {
-  //create the table header section
-  var thEl = document.createElement('th');
-  tableEl.appendChild(thEl);
-  //create a table row
-  var trEl = document.createElement('tr');
-  thEl.appendChild(trEl);
-  //create a td cell
-  var tdEl = document.createElement('td');
-  trEl.appendChild(tdEl);
-
-  //create the table body section
-  var tbodyEl = document.createElement('tbody');
-  tableEl.appendChild(tbodyEl);
-  //create a table row
-  trEl = document.createElement('tr');
-  tbodyEl.appendChild(trEl);
-  //create a td cell
-  tdEl = document.createElement('td');
-  trEl.appendChild(tdEl);
-
-  //create the table footer section
-  var tfootEl = document.createElement('tfoot');
-  tableEl.appendChild(tfootEl);
-  //create a table row
-  trEl = document.createElement('tr');
-  tfootEl.appendChild(trEl);
-  //create a td cell
-  tdEl = document.createElement('td');
-  trEl.appendChild(tdEl);
+Shop.prototype.calcCookiesSoldEachHour = function(minCustomer, maxCustomer, avgCookies){
+  for(var i = 0; i < hoursOpen.length; i++){
+    //for each hour open, get random number of customers
+    var customersEachHour = Math.random() * (maxCustomer - minCustomer) + minCustomer;
+    //calculate cookies each hour and append to array
+    var cookiesEachHour = Math.floor(customersEachHour * avgCookies);
+    console.log('shop name is ' + this.name);
+    this.cookiesEachHourArr.push(cookiesEachHour);
+  }
 };
 
 new Shop('1st and Pike', 23, 65, 6.3);
@@ -52,36 +32,59 @@ new Shop('Seattle Center', 11, 38, 3.7);
 new Shop('Capitol Hill', 20, 38, 2.3);
 new Shop('Alki', 2, 16,4.6);
 
-Shop.prototype.calcCookiesSoldEachHour = function(minCustomer, maxCustomer, avgCookies){
-  for(var i = 0; i < hoursOpen.length; i++){
-    //for each hour open, get random number of customers
-    var customersEachHour = Math.random() * (maxCustomer - minCustomer) + minCustomer;
-    //calculate cookies each hour and append to array
-    var cookiesEachHour = Math.floor(customersEachHour * avgCookies);
-  }
-  this.cookiesEachHourArr.push(cookiesEachHour);
-};
+console.log(allShops);
 
 //render header
-function renderHeader(){
+function renderTable() {
   //create header section
   var theadEl = document.createElement('thead');
   tableEl.appendChild(theadEl);
-  //create row
+  //create header row
   var trEl = document.createElement('tr');
   theadEl.appendChild(trEl);
   //create blank td
   var tdEl = document.createElement('td');
   trEl.appendChild(tdEl);
-  //for each hour open, create td
+  //for each hour open, create and fill td
   for(var i = 0; i < hoursOpen.length; i++) {
     tdEl = document.createElement('td');
     tdEl.textContent = hoursOpen[i];
     trEl.appendChild(tdEl);
   }
+  //create the table body section
+  var tbodyEl = document.createElement('tbody');
+  tableEl.appendChild(tbodyEl);
+
+  //for each shop in the allShops array,
+  for(var s =  0; s < allShops.length; s++) {
+    console.log(allShops[s].name);
+
+    //calcCookiesSoldEachHour to fill cookiesEachHourArr
+    allShops[s].calcCookiesSoldEachHour(allShops[s].minCustomer, allShops[s].maxCustomer, allShops[s].avgCookiesPerSale);
+
+    //create a row and append to tbody
+    trEl = document.createElement('tr');
+    tbodyEl.appendChild(trEl);
+
+    //create and fill first td with shop name
+    tdEl = document.createElement('td');
+    tdEl.textContent = allShops[s].name;
+
+    //for each item in the shop's cookiesEachHourArr
+    for(var h = 0; h < allShops[s].cookiesEachHourArr.length; h++) {
+
+      //create and fill td
+      tdEl = document.createElement('td');
+      tdEl.textContent = allShops[s].cookiesEachHourArr[h];
+      //append to row
+      trEl.appendChild(tdEl);
+    }
+  }
+
 }
 
-renderHeader();
+
+renderTable();
 //render shops rows
 //render totals footer row
 ////////////////////////////////////////////////////////////////////////////
